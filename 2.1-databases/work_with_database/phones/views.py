cf8bbe2c51bbcbf8014284114ecb16d8055cb00a
RomashkinKirill <romashkin-92@ya.ru>
2024-05-12 21:43:51 +0300

    HW_3

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,28 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort_order = request.GET.get('sort')
+    phones = Phone.objects.all()
+    if sort_order == 'name':
+        phones = phones.order_by('name')
+    elif sort_order == 'min_price':
+        phones = phones.order_by('price')
+    elif sort_order == 'max_price':
+        phones = phones.order_by('-price')
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    context = {'phones': phones}
+    return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.filter(slug__contains=slug).first()
+    context = {'phone': phone}
+    return render(request, template, context=context)
