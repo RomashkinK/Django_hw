b136ba41db87444b33f2ce1e504c10833ec74cf1
RomashkinKirill <romashkin-92@ya.ru>
2024-05-26 10:11:44 +0300

    HW_4

--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,5 +1,16 @@
+
 from django.db import models
 
+
+class Tag(models.Model):
+    
+    name = models.CharField(max_length=50, verbose_name='Название')
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
 
@@ -7,10 +18,24 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
-
+    tags = models.ManyToManyField(Tag, related_name='articles', through='Scope')
+    
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
-
+        ordering = ['-published_at']
+        
     def __str__(self):
         return self.title
+    
+    
+class Scope(models.Model):
+    
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(verbose_name='Основной')
+    
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+    
